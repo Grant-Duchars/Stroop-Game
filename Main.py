d12759823ad226c81d31b365934b017d63e6efdd
Grant Duchars <spam.th3.ham@gmail.com>
2017-07-27 10:22:02 -0500

    added functional buttons to main menu

--- Main.py
+++ Main.py
@@ -9,16 +9,44 @@ class Main:
         self.height = 800
         # Game clock
         self.clock = pygame.time.Clock()
-    
+        # 1 = Game, 2 = Main Menu, 3 = Settings, 4 = Credits
+        self.currentScreen = 2
+
+    def detectClick(self):
+        self.mousePos = pygame.mouse.get_pos()
+        return self.mousePos
+        # print(self.mousePos)
+
     def runGame(self):
         '''Starts the game'''
         self.main_menu = MainMenu.Main_Menu(self.width,self.height)
-        self.clock.tick
+        self.main_menu.runScreen()
+        
         while True:
+            self.clock.tick(10)
             events = pygame.event.get()
             for event in events:
                 if event.type == pygame.QUIT:
                     sys.exit()
+            if self.currentScreen == 1:
+                pass
+            elif self.currentScreen == 2:
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if self.detectClick()[0] >= 87 and self.detectClick()[1] >= 500 and self.detectClick()[0] <= 195 and self.detectClick()[1] <= 540:
+                        self.currentScreen = 1
+                        print("Clicked Play")
+                    if self.detectClick()[0] >= 87 and self.detectClick()[1] >= 550 and self.detectClick()[0] <= 290 and self.detectClick()[1] <= 590:
+                        self.currentScreen = 3
+                        print("Clicked Settings")
+                    if self.detectClick()[0] >= 87 and self.detectClick()[1] >= 610 and self.detectClick()[0] <= 260 and self.detectClick()[1] <= 640:
+                        self.currentScreen = 4
+                        print("Clicked Credits")
+            elif self.currentScreen == 3:
+                pass
+            elif self.currentScreen == 4:
+                pass
+            if pygame.mouse.get_pressed()[0] == 1:
+                self.detectClick()
 
 def main():
     pygame.init()
--- source/MainMenu.py
+++ source/MainMenu.py
@@ -4,16 +4,26 @@ class Main_Menu:
 
     def __init__(self, width, height):
         '''Starts the main menu screen'''
+        self.width = width
+        self.height = height
         self.screen = pygame.display.set_mode((width,height))
-        self.screen_size = self.screen.get_size()
-        # self.width = self.screen_size.x
         self.background = pygame.Surface(self.screen.get_size())
         self.background = self.background.convert()
         self.screen_color = (255,255,255)
-        self.screen.fill(self.screen_color)
         pygame.display.set_caption("Stroop Game")
+        # Fonts
         self.titleFont = pygame.font.Font("fonts/Square.ttf", 70)
-        self.font = pygame.font.Font("fonts/Square.ttf", 30)
+        self.font = pygame.font.Font("fonts/Square.ttf", 50)
+        # Text
         self.text1 = self.titleFont.render("Stroop Game", 1, pygame.Color("black"))
-        self.screen.blit(self.text1, (20,20))
+        self.text2 = self.font.render("Play", 1, pygame.Color("black"))
+        self.text3 = self.font.render("Settings", 1, pygame.Color("black"))
+        self.text4 = self.font.render("Credits", 1, pygame.Color("black"))
+     
+    def runScreen(self):
+        self.screen.fill(self.screen_color)
+        self.screen.blit(self.text1, (85,100))
+        self.screen.blit(self.text2, (85,500))
+        self.screen.blit(self.text3, (85,550))
+        self.screen.blit(self.text4, (85,600))
         pygame.display.update()
